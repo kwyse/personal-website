623eab22699d7730d4b1d0d53c4ad091765e5cd9
Krishan Wyse <kwysek@gmail.com>
2016-08-20 21:33:20 +0100

    Add script to publish blog post

--- src/db.rs
+++ src/db.rs
@@ -5,21 +5,13 @@ use diesel::pg::PgConnection;
 
 use self::models::*;
 
-pub fn establish_connection() -> PgConnection {
-    use dotenv::dotenv;
-    use std::env;
-
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
-}
-
 pub fn create_post(post: NewBlogPost) -> BlogPost {
     use db::schema::blogposts;
 
     let conn = establish_connection();
-    ::diesel::insert(&post).into(blogposts::table).get_result(&conn).expect("Error adding new post")
+    ::diesel::insert(&post).into(blogposts::table)
+        .get_result(&conn)
+        .expect("Error adding new post")
 }
 
 pub fn read_posts() -> Vec<BlogPost> {
@@ -27,11 +19,33 @@ pub fn read_posts() -> Vec<BlogPost> {
     use db::schema::blogposts::dsl::*;
 
     let conn = establish_connection();
-    blogposts.load::<BlogPost>(&conn).unwrap_or_else(|err| {
-        warn!("Error loading blog posts from database - returning empty Vec");
-        error!("{}", err.description());
-        Vec::new()
-    })
+    blogposts.filter(published.eq(true))
+        .order(created.desc())
+        .load::<BlogPost>(&conn).unwrap_or_else(|err| {
+            warn!("Error loading blog posts from database - returning empty Vec");
+            error!("{}", err.description());
+            Vec::new()
+        })
+}
+
+pub fn publish_post(target_id: i32) -> BlogPost {
+    use db::schema::blogposts::dsl::*;
+
+    let conn = establish_connection();
+    ::diesel::update(blogposts.filter(id.eq(target_id)))
+        .set(published.eq(true))
+        .get_result(&conn)
+        .expect("Error publishing post")
+}
+
+fn establish_connection() -> PgConnection {
+    use dotenv::dotenv;
+    use std::env;
+
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
 }
 
 pub mod models {
